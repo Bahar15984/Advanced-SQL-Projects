33411c609e4e699adba9bfe0eb262be0bb906854
BAHAR ALMASI <baharalmasi@BAHARs-MacBook-Air.local>
2025-09-09 15:17:35 -0400

    Rename bank_analysis_part1.sql to bank_analysis.sql

---
+++ analysis/product_sales_analysis.sql
@@ -0,0 +1,23 @@
+/* POS sanity checks */
+SELECT TOP 10 * FROM dbo.POS1 ORDER BY [date];
+SELECT TOP 10 * FROM dbo.POS2 ORDER BY [date];
+SELECT TOP 10 * FROM dbo.POS3 ORDER BY [date];
+SELECT TOP 10 * FROM dbo.Sales_Summary ORDER BY [date];
+
+/* Total sales per product */
+SELECT s.product, SUM(s.sales) AS total_sales
+FROM dbo.Sales_Summary s
+GROUP BY s.product
+ORDER BY total_sales DESC;
+
+/* Monthly sales trend */
+SELECT DATEFROMPARTS(YEAR([date]), MONTH([date]), 1) AS month_start,
+       SUM(sales) AS month_sales
+FROM dbo.Sales_Summary
+GROUP BY DATEFROMPARTS(YEAR([date]), MONTH([date]), 1)
+ORDER BY month_start;
+
+/* Top N selling days per product */
+SELECT product, [date], sales,
+       ROW_NUMBER() OVER (PARTITION BY product ORDER BY sales DESC) AS rn
+FROM dbo.Sales_Summary;
